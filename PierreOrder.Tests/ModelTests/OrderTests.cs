f71e8d799c030f93c00d37f2244fd5a4d9cb036a
John Lenz <john.lenz27@gmail.com>
2023-03-03 10:03:13 -0800

    good test 4 fail on creating a date instance

--- PierreOrder.Tests/ModelTests/OrderTests.cs
+++ PierreOrder.Tests/ModelTests/OrderTests.cs
@@ -12,14 +12,14 @@ namespace PierreOrder.Tests
         [TestMethod]
         public void ItemConstructor_CreatesInstanceOfItem_Item()
         {
-            Order newOrder = new Order("test", "test");
+            Order newOrder = new Order("test", "test", "test");
             Assert.AreEqual(typeof(Order), newOrder.GetType());
         }
         [TestMethod]
         public void GetName_ReturnsName_String()
         {
             string name = "flour";
-            Order newOrder = new Order(name, "test");
+            Order newOrder = new Order(name, "test", "test");
             string result = newOrder.Name;
             Assert.AreEqual(name, result);
         }
@@ -29,9 +29,20 @@ namespace PierreOrder.Tests
         {
             string name = "flour";
             string description = "2 bags";
-            Order newOrder = new Order(name, description);
+            Order newOrder = new Order(name, description, "test");
             string result = newOrder.Description;
             Assert.AreEqual(description, result);
+        }
+
+           [TestMethod]
+        public void GetDate_ReturnsDate_string()
+        {
+            string name = "flour";
+            string description = "2 bags";
+            string date = "03-03-23";
+            Order newOrder = new Order(name, description, date);
+            string result = newOrder.date;
+            Assert.AreEqual(date, result);
         }
     }
 
